7fe85a49789de16ac9702e56b315dce4ae89e74b
A Ray <a@machinaut.com>
2019-06-21 21:18:00 -0700

    more point tests

--- parser.py
+++ parser.py
@@ -3,17 +3,22 @@
 import argparse
 import re
 
-point_prog = re.compile(r'\s*\(\s*(\d+)\s*,\s*(\d+)\s*\)\s*')
+point_prog = re.compile(r'^\s*\(\s*(\d+)\s*,\s*(\d+)\s*\)\s*')
 
 
 def parse_point(s):
     result = point_prog.match(s)
     a, b = result.groups()
-    return int(a), int(b)
+    return (int(a), int(b)), s[result.span()[1]:]
+
+
+def parse_map(s):
+    # do parse_point while maybe_consume_comma
+    pass
 
 
 def main(file='tests/test1.desc'):
-    print('hi')
+    parse_point('(1,2)')
 
 
 if __name__ == '__main__':
--- test_parser.py
+++ test_parser.py
@@ -9,11 +9,12 @@ class TestParser(unittest.TestCase):
     def check_point_prog(self, s, x, y):
         result = point_prog.match(s)
         self.assertIsNotNone(result)
-        a, b = result.groups()
+        (a, b) = result.groups()
         self.assertEqual(x, int(a))
         self.assertEqual(y, int(b))
-        self.assertEqual((x, y), parse_point(s))
 
+    def check_parse_point(self, s, x, y, rem):
+        self.assertEqual(((x, y), rem), parse_point(s))
 
     def test_point_prog(self):
         self.check_point_prog('(1,2)', 1, 2)
@@ -23,6 +24,9 @@ class TestParser(unittest.TestCase):
         self.check_point_prog('(1, 2)', 1, 2)
         self.check_point_prog('(1,2 )', 1, 2)
         self.check_point_prog('(1,2) ', 1, 2)
+        self.check_point_prog('(1,2) ', 1, 2)
+        self.check_point_prog('(1,2)', 1, 2)
+        self.check_parse_point('(1,2),(3, 4)', 1, 2, ',(3, 4)')
 
 
 if __name__ == '__main__':
